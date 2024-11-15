8647bc27043eb57a2152cf208ba4c512aae0b2d5
Labib Makarim <labibmakarim197@gmail.com>
2024-11-15 10:16:15 +0700

    Sale delete

--- app/Livewire/Sale/SaleList.php
+++ app/Livewire/Sale/SaleList.php
@@ -3,6 +3,7 @@
 namespace App\Livewire\Sale;
 
 use App\Models\Sale;
+use Illuminate\Support\Facades\DB;
 use Livewire\Attributes\Title;
 use Livewire\Component;
 
@@ -18,5 +19,12 @@ class SaleList extends Component
     public function render()
     {
         return view('livewire.sale.sale');
+    }
+
+    public function delete(int $id)
+    {
+        DB::delete("DELETE FROM sales WHERE id = $id;");
+
+        return $this->sales = Sale::all();
     }
 }
