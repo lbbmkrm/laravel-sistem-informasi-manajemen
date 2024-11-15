927fb0ae58611f7957f12d8aeb296584981ef95c
Labib Makarim <labibmakarim197@gmail.com>
2024-11-15 13:22:41 +0700

    User Test

---
+++ tests/Feature/UserTest.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Database\Seeders\DatabaseSeeder;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Log;
+use Livewire\Livewire;
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testUserList()
+    {
+        $this->seed(DatabaseSeeder::class);
+        $user = User::where('email', 'admin@example.com')->first();
+        $this->actingAs($user)->get('/users')
+            ->assertStatus(200)
+            ->assertSeeText('admin@example.com');
+    }
+
+    public function testUserCreate()
+    {
+        Livewire::test('user.user-create')
+            ->set('name', 'Test')
+            ->set('email', 'test@test.com')
+            ->set('role', true)
+            ->set('password', 'password123')
+            ->call('create');
+
+        $this->assertDatabaseHas('users', [
+            'name' => 'Test',
+            'email' => 'test@test.com',
+        ])->assertDatabaseCount('users', 1);
+    }
+
+
+    public function testUserUpdate()
+    {
+        $this->seed(DatabaseSeeder::class);
+
+        self::assertDatabaseHas('users', [
+            'name' => 'Admin User'
+        ]);
+
+        $user = User::where('email', 'admin@example.com')->first();
+        Log::info(json_encode($user));
+
+        Livewire::test('user.user-update', ['id' => $user->id])
+            ->set('name', 'Admin Updated')
+            ->set('email', 'admin.update@example.com')
+            ->call('update');
+
+        $this->assertDatabaseHas('users', [
+            'name' => 'Admin Updated',
+            'email' => 'admin.update@example.com'
+        ]);
+    }
+
+    public function testUserDelete()
+    {
+        $this->seed(DatabaseSeeder::class);
+        self::assertDatabaseCount('users', 5);
+
+        $adminUser = User::where('is_admin', true)->first();
+        $this->actingAs($adminUser);
+        $user = User::where('name', 'John Doe')->first();
+
+        Livewire::test('user.UserList')
+            ->call('delete', $user->id);
+
+        self::assertDatabaseCount('users', 4);
+    }
+}
